8855db3c86db82afe18952471101fa839f5b0feb
MashaTvile <s225084@student.dtu.dk>
2023-10-24 09:51:48 +0200

    Added data object: triplets

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Triplets
+#'
+#' All triplets coding for all amino acids. 3 x bases --> 1 AA
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"triplets"
